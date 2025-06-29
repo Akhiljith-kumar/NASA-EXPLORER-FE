b926e8ed770fd137634159dbdaf25906ed32a541
akhiljith <akhiljith.kumar.2025@mumail.ie>
2025-06-29 19:10:23 +0100

    responsvie for small devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import './components.css'
 const Header = () => {
 
   return (
-     <div className='flex-row'>
+     <div className='flex-row main-head-wrapper'>
           <img className='astronaut-icon App-logo' src="icons/atronaut-svgrepo-com.svg" alt="" />
           <p className='main-head'><span className='orange'>NASA</span> EXPLORER</p>
      </div>
