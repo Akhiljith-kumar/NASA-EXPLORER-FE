26b23902a870388d1f8f85f294ce4f3786cb6f60
akhiljith <akhiljith.kumar.2025@mumail.ie>
2025-06-29 23:07:17 +0100

    local font ttf

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
      <div className='flex-row main-head-wrapper'>
           <img className='astronaut-icon App-logo' src="icons/atronaut-svgrepo-com.svg" alt="" />
-          <p className='main-head'><span className='orange'>NASA</span> EXPLORER</p>
+          <p className='main-head font-cubano'><span className='orange'>NASA</span> EXPLORER</p>
      </div>
   );
 };
